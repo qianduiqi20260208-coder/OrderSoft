25734df7729316e663deba46e2c81388a1790d77
Allan <1320149730@qq.com>
2025-08-26 11:08:40 +0800

    修改

--- fantastic-admin/src/router/model/tab.ata36.penumatic.ts
+++ fantastic-admin/src/router/model/tab.ata36.penumatic.ts
@@ -11,7 +11,7 @@ const routes: RouteRecordRaw = {
   meta: {
     title: 'ATA36_气源系统仿真模型', // 菜单或标签页显示的标题
     icon: 'i-ic:round-tab', // 菜单或标签页显示的图标
-    auth: 'ATA36_Pneumatic.browse', // 需要的具体权限
+    auth: 'ATA36_Penumatic.browse', // 需要的具体权限
   },
   children: [
     {
