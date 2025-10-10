4aa02953d181b0b0bc7824810923c0ae58c0ece7
hyx <1833613178@qq.com>
2025-10-10 14:32:39 +0800

    fix: 移除问题订单创建时文件上传的必填限制
    移除问题订单创建表单中参考文件的必填标记和验证，以及提交按钮的相关禁用逻辑，使其成为可选字段

--- fantastic-admin/src/views/order/order_create.vue
+++ fantastic-admin/src/views/order/order_create.vue
@@ -68,7 +68,6 @@ async function submitProblemOrder() {
     || !problemOrderForm.value.modelVersionID
     || !problemOrderForm.value.description
     || !problemOrderForm.value.approverID
-    || problemOrderForm.value.files.length === 0
   ) {
     ElMessage.error('请完整填写所有必填项')
     return
@@ -1075,7 +1074,7 @@ async function fetchCustomerList() {
               placeholder="请输入备注"
             />
           </el-form-item>
-          <el-form-item label="参考文件" required>
+          <el-form-item label="参考文件">
             <el-upload
               v-model:file-list="problemOrderForm.files"
               action="#"
@@ -1096,7 +1095,7 @@ async function fetchCustomerList() {
           </el-button>
           <el-button
             type="primary"
-            :disabled="!problemOrderForm.modelId || !problemOrderForm.description || !problemOrderForm.modelVersionID || !problemOrderForm.approverID || problemOrderForm.files.length === 0"
+            :disabled="!problemOrderForm.modelId || !problemOrderForm.description || !problemOrderForm.modelVersionID || !problemOrderForm.approverID"
             @click="submitProblemOrder"
           >
             提交
